85a9d83757be32d23563ed589ba1d57a516fbd20
Ifeoluwa <ifel1939@gmail.com>
2022-10-30 15:36:42 +0100

    never be in doubt

--- translator.js
+++ translator.js
@@ -2,3 +2,15 @@
 
 
 let container=document.querySelector(".container")
+let interchange=document.querySelector(".interchange")
+let location=document.querySelector(".location")
+let selectBox=document.querySelector(".select-box")
+let select=document.querySelector("select")
+let exchangeIcon=document.querySelector(".exchange-icon")
+let position=document.querySelector(".position")
+let inputContainer=document.querySelector(".input-text")
+let resultContainer=document.querySelector(".result-text")
+let inputTextArea=document.querySelector(".from-text")
+let resultTextArea=document.querySelector(".to-text")
+let copyLogo=document.querySelector("#copy")
+let audioLogo=document.querySelector(".audio")
