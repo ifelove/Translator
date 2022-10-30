e33733297de5d4c171526bcd36d91c980ca76f49
Ifeoluwa <ifel1939@gmail.com>
2022-10-30 15:42:41 +0100

    little arrangement in the  html file

--- translator.js
+++ translator.js
@@ -1,16 +1,16 @@
 //https://api.mymemory.translated.net/get?q=Hello World!&langpair=en|it
 
-
-let container=document.querySelector(".container")
-let interchange=document.querySelector(".interchange")
-let location=document.querySelector(".location")
-let selectBox=document.querySelector(".select-box")
-let select=document.querySelector("select")
-let exchangeIcon=document.querySelector(".exchange-icon")
-let position=document.querySelector(".position")
-let inputContainer=document.querySelector(".input-text")
-let resultContainer=document.querySelector(".result-text")
-let inputTextArea=document.querySelector(".from-text")
-let resultTextArea=document.querySelector(".to-text")
-let copyLogo=document.querySelector("#copy")
-let audioLogo=document.querySelector(".audio")
+let container = document.querySelector(".container");
+let interchange = document.querySelector(".interchange");
+let location = document.querySelector(".location");
+let selectBox = document.querySelector(".select-box");
+let select = document.querySelector("select");
+let exchangeIcon = document.querySelector(".exchange-icon");
+let position1 = document.querySelector(".position1");
+let position2 = document.querySelector(".position2");
+let inputContainer = document.querySelector(".input-text");
+let resultContainer = document.querySelector(".result-text");
+let inputTextArea = document.querySelector(".from-text");
+let resultTextArea = document.querySelector(".to-text");
+let copyLogo = document.querySelector("#copy");
+let audioLogo = document.querySelector(".audio");
